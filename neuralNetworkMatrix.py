7049f029967179e4cf0a2b0f703eb4b859d7e5ec
yooj425165 <26_34@bssm.hs.kr>
2026-09-08 23:57:17 +0900

    4일차

--- neuralNetworkMatrix.py
+++ neuralNetworkMatrix.py
@@ -1,12 +1,17 @@
 import numpy as np
 import matplotlib.pylab as plt
-X = np.array([1,2])
-print(X.shape)
-W = np.array([[1,3,5],[2,4,6]])
-print(W.shape)
-Y = np.dot(X,W)
-print(Y)
 
-plt.plot()
-plt.show()
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+X1 = np.array([1.0,0.5])
+W1 = np.array([[0.1,0.3,0.5],[0.2,0.4,0.6]])
+B1 = np.array([0.1,0.2,0.3])
+
+A1 = np.dot(X1,W1) + B1
+
+Z1 = sigmoid(A1)
+
+print(A1)
+print(Z1)
 
