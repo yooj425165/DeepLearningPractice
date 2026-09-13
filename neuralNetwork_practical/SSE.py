bbf824c0fd19b29c08364cb1f0c681782fcd5e92
yooj425165 <26_34@bssm.hs.kr>
2026-09-13 23:13:47 +0900

    loss function_1

---
+++ neuralNetwork_practical/SSE.py
@@ -0,0 +1,7 @@
+import numpy as np
+def sum_squares_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
+t = [0,0,1,0,0,0,0,0,0,0]
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+print(sum_squares_error(np.array(y), np.array(t)))
